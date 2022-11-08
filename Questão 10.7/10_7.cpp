dff7b1762b087e98a9d3794c28937afe3b408384
Israel Benvindo <75487136+is-benvind@users.noreply.github.com>
2022-11-08 00:19:36 -0300

    Add files via upload

---
+++ Questão 10.7/10_7.cpp
@@ -0,0 +1,20 @@
+#include "polinomio.cpp"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    Poli n = termo(3, 5, termo(-6, 2, termo(1, 0, NULL)));
+    exibep(n);
+
+    cout << endl;
+
+    Poli g = termo(1, 5, termo(2, 1, termo(1, 0, NULL)));
+    exibep(g);
+    cout << endl;
+
+    Poli t = soma(n, g);
+
+    exibep(t);
+}
