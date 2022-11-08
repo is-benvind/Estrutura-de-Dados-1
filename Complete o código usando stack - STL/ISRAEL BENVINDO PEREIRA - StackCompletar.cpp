3510c31b2f59eb36bd7556c66fe645311d71ef89
Israel Benvindo <75487136+is-benvind@users.noreply.github.com>
2022-11-08 12:16:18 -0300

    Add files via upload

---
+++ Complete o código usando stack - STL/ISRAEL BENVINDO PEREIRA - StackCompletar.cpp
@@ -0,0 +1,54 @@
+#include <stack>
+#include <iostream>
+using namespace std;
+
+template <class T>
+class Stack: public stack<T> {
+	protected:
+		stack<T> pilha;
+	public:
+	T popStack(){
+		T tmp=pilha.top();
+		pilha.pop();
+		return tmp;
+	}
+	
+	
+	void popTodos(){
+		while(!pilha.empty()){
+			cout << pilha.top() << end1;
+			pilha.pop();
+		}
+    }
+	
+	void simulaFila(T novo){
+		stack<T> pilhaAux;
+		if(pilha.empty()){
+			pilha.push(novo);
+		} else {
+			while(!pilha.empty()){
+				pilhaAux.push(pilha.top());
+				pilha.pop();
+			}
+			pilha.push(novo);
+			
+			while(!pilhaAux.empty()){
+				pilha.push(pilhaAux.top());
+				pilhaAux.pop();
+			}
+		}
+				
+	}
+	
+	
+};
+
+main(){
+	Stack<int> pilha;
+	pilha.simulaFila(1);
+	pilha.simulaFila(2);
+	pilha.simulaFila(3);
+	
+	//cout << fil.popQueue() << end1;
+	pilha.popTodos(); 
+}
